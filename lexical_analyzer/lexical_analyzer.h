693c3565a27d9bf3d5656b760f2b1a28b0935999
peterglezroa <peterglezroa@gmail.com>
2021-10-17 16:14:10 -0500

    Finish FIRST function for lexical analyzer

--- lexical_analyzer/lexical_analyzer.h
+++ lexical_analyzer/lexical_analyzer.h
@@ -6,71 +6,168 @@
 #include <algorithm>
 #include <map>
 #include <list>
+#include <exception>
 #include <regex>
 
 //#define RULEREGEX "([A-Za-z_-]+) ?-> ?(.*)"
+//
+class Production {
+  public:
+    std::string variable;
+    std::list<std::string> elements;
+
+    Production(std::string variable) : variable(variable) {}
+
+    std::string toString() {
+      std::string str = variable;
+      str.append(" -> ");
+      for (const std::string elem : elements) {
+        str.append(elem);
+        str.append(" ");
+      }
+      str.pop_back();
+      return str;
+    }
+};
 
 class LexicalAnalyzer {
-    private:
-        std::list<std::string> vars; // Syntathic variables
-        std::list<std::string> terms; // Terminals
+  private:
+    std::list<std::string> vars; // Syntathic variables
+    std::list<std::string> terms; // Terminals
+    std::list<Production> prods; // All productions
 
-        bool has_var(std::string str) {
-            return (std::find(vars.begin(), vars.end(), str)!=vars.end());
+    bool has_var(std::string str) {
+      /* Returns a boolean that confirms if the received string is part of the
+       * synthatic variables */
+      return (std::find(vars.begin(), vars.end(), str)!=vars.end());
+    }
+
+    bool has_term(std::string str) {
+      /* Returns a boolean that confirms if the received string is part of the
+       * terminals */
+      return (std::find(terms.begin(), terms.end(), str)!=terms.end());
+    }
+
+    std::list<Production> productions(std::string var) {
+      /* Returns a list of productions where the received string is the
+       * variable */
+      std::list<Production> list;
+      for (Production prod : prods)
+        if (var.compare(prod.variable) == 0)
+          list.push_back(prod);
+
+      return list;
+    }
+
+    std::list<std::string> first(std::string str) {
+      std::list<std::string> firsts;
+
+      // First rule: first of a terminal
+      if (has_term(str) || str.compare("''") == 0) firsts.push_back(str);
+      else if (has_var(str)) {
+        for (const Production production : prods) {
+          if (str.compare(production.variable) == 0 &&
+              str.compare(production.elements.front()) != 0) {
+            // First of the first element of the production
+            std::list<std::string> ff = first(production.elements.front());
+
+            // Find if it has epsilon
+            if (std::find(ff.begin(), ff.end(), "''") != ff.end() &&
+                production.elements.size() > 1)
+              // Also add the first of the following element
+              ff.splice(ff.end(), first(*std::next(production.elements.begin())));
+
+            firsts.splice(firsts.end(), ff);
+          }
         }
+      } else {
+        fprintf(stderr, "Not part of synthatic variabels or terminals (%s)!\n", str.c_str());
+        throw "Not part of synthatic variables or terminals!";
+      }
 
-        bool has_term(std::string str) {
-            return (std::find(terms.begin(), terms.end(), str)!=terms.end());
-        }
+      firsts.unique(); // Clean List
+      return firsts;
+    }
 
-    public:
-        LexicalAnalyzer() {}
+    std::list<std::string> follow(std::string str) {
+      std::list<std::string> firsts;
+      return firsts;
+    }
 
-        void clear() { vars.clear(); terms.clear(); }
+  public:
+    LexicalAnalyzer() {}
 
-        bool parse(std::string production) {
-            /* Parses a given production. If the sintax is valid it returns true, if
-             * not it returns false */
-            std::regex productionRegex("([A-Za-z_-]+) -> (.*)");
-            std::string variable, terminal, *strptr;
-            char *cptr;
-            size_t pos;
+    void clear() { vars.clear(); terms.clear(); }
 
-            if (!std::regex_match(production, productionRegex))
-                return false;
+    bool parse(std::list<std::string> productions) {
+      bool all = true;
+      for (std::string production : productions)
+        if (!parse(production))
+          all = false;
+      return all;
+    }
 
-            // Find the divider between variable and terminals
-            pos = production.find(" -> ");
+    bool parse(std::string production) {
+      /* Parses a given production. If the sintax is valid it returns true, if
+       * not it returns false */
+      std::regex productionRegex("([A-Za-z_-]+) -> (.*)");
+      std::string variable, terminal, *strptr;
+      char *cptr;
+      size_t pos;
 
-            // Get variable and add it to list if not found
-            variable = production.substr(0, pos);
-            if (!has_var(variable))
-                vars.push_back(variable);
+      if (!std::regex_match(production, productionRegex))
+        return false;
 
-            // Remove variable from terminal list if it was inside
-            terms.remove(variable);
+      // Find the divider between variable and terminals
+      pos = production.find(" -> ");
 
-            production.erase(0, pos+4); // Erase no terminal to only leave rule
+      // Get variable and add it to list if not found
+      variable = production.substr(0, pos);
+      if (!has_var(variable))
+        vars.push_back(variable);
+      Production prod = Production(variable);
 
-            // Iterate through words in the production
-            while ((pos = production.find(' ')) != std::string::npos) {
-                terminal = production.substr(0, pos);
-                if (!has_var(terminal) && !has_term(terminal))
-                    terms.push_back(terminal);
-                production.erase(0, pos+1);
-            }
-            terms.push_back(production); // last word
+      // Remove variable from terminal list if it was inside
+      terms.remove(variable);
 
-            terms.unique();
-            terms.remove("''");
-            return true;
-        }
+      production.erase(0, pos+4); // Erase no terminal to only leave rule
 
-        bool validString(std::string) {return false;}
+      // Iterate through words in the production
+      while ((pos = production.find(' ')) != std::string::npos) {
+        terminal = production.substr(0, pos);
+        if (!has_var(terminal))
+          terms.push_back(terminal);
+        prod.elements.push_back(terminal);
+        production.erase(0, pos+1);
+      }
+      terms.push_back(production); // last word
+      prod.elements.push_back(production);
 
-        const std::list<std::string> getVariables() const { return vars;}
+      prods.push_back(prod);
+      terms.unique();
+      terms.remove("''");
+      return true;
+    }
 
-        const std::list<std::string> getTerminals() const { return terms; }
+    bool validString(std::string) {return false;}
+
+    std::string toString() {
+      std::string str = "";
+      for (Production prod : prods) {
+        str.append(prod.toString());
+        str.append("\n");
+      }
+      str.pop_back();
+      return str;
+    }
+
+    const std::list<std::string> getVariables() const { return vars;}
+
+    const std::list<std::string> getTerminals() const { return terms; }
+
+    std::list<std::string> getFirst(std::string str) { return first(str); }
+
+    std::list<std::string> getFollow(std::string str) { return follow(str); }
 };
 
 #endif
--- lexical_analyzer/test.cpp
+++ lexical_analyzer/test.cpp
@@ -5,135 +5,227 @@
 #include "lexical_analyzer.h"
 
 void compare_lists (
-    const std::list<std::string> list1,
-    const std::list<std::string> list2
+  const std::list<std::string> list1,
+  const std::list<std::string> list2
 ){
-    std::queue<std::string> missing; // List of words missing
+  std::queue<std::string> missing; // List of words missing
 
-    // Find items of list 1 in list 2
-    for (std::string str : list1)
-        if (std::find(list2.begin(), list2.end(), str) == list2.end())
-            missing.push(str);
+  // Find items of list 1 in list 2
+  for (std::string str : list1)
+    if (std::find(list2.begin(), list2.end(), str) == list2.end())
+      missing.push(str);
 
-    // Find items of list 2 in list 1
-    for (std::string str : list2)
-        if (std::find(list1.begin(), list1.end(), str) == list1.end())
-            missing.push(str);
+  // Find items of list 2 in list 1
+  for (std::string str : list2)
+    if (std::find(list1.begin(), list1.end(), str) == list1.end())
+      missing.push(str);
 
-    if (missing.size() > 0) {
-        fprintf(stdout, "\033[1;31mINCORRECT\033[0m\n\t");
+  if (missing.size() > 0) {
+    fprintf(stdout, "\033[1;31mINCORRECT\033[0m\n\t");
 
-        fprintf(stdout, "( ");
-        std::string top = missing.front(); missing.pop();
-        for (const std::string str : list1)
-            if (str.compare(top) == 0) {
-                fprintf(stdout, "\033[1;35m%s \033[0m", str.c_str());
-                if (!missing.empty()) { top = missing.front(); missing.pop(); }
-            } else fprintf(stdout, "%s ", str.c_str());
-        fprintf(stdout, ")");
+    fprintf(stdout, "( ");
+    std::string top = missing.front(); missing.pop();
+    for (const std::string str : list1)
+      if (str.compare(top) == 0) {
+        fprintf(stdout, "\033[1;35m%s \033[0m", str.c_str());
+        if (!missing.empty()) { top = missing.front(); missing.pop(); }
+      } else fprintf(stdout, "%s ", str.c_str());
+    fprintf(stdout, ")");
 
-        fprintf(stdout, "\t( ");
-        for (const std::string str : list2)
-            if (str.compare(top) == 0) {
-                fprintf(stdout, "\033[1;35m%s \033[0m", str.c_str());
-                if (!missing.empty()) { top = missing.front(); missing.pop(); }
-            } else fprintf(stdout, "%s ", str.c_str());
-        fprintf(stdout, ")\n");
-    } else fprintf(stdout, "\033[1;32mCORRECT\033[0m\n");
+    fprintf(stdout, "\t( ");
+    for (const std::string str : list2)
+      if (str.compare(top) == 0) {
+        fprintf(stdout, "\033[1;35m%s \033[0m", str.c_str());
+        if (!missing.empty()) { top = missing.front(); missing.pop(); }
+      } else fprintf(stdout, "%s ", str.c_str());
+    fprintf(stdout, ")\n");
+  } else fprintf(stdout, "\033[1;32mCORRECT\033[0m\n");
 }
 
 int main(int argc, char *argv[]) {
-    LexicalAnalyzer analyzer = LexicalAnalyzer();
+  LexicalAnalyzer analyzer = LexicalAnalyzer();
 
-    fprintf(stdout, "===================== TEST 01 =====================\n");
-    std::list<std::string> test1p = {"E", "EPrime", "T", "TPrime", "F"};
-    std::list<std::string> test1t = {"+", "*", "(", ")", "id"};
-    analyzer.parse("E -> T EPrime");
-    analyzer.parse("EPrime -> + T EPrime");
-    analyzer.parse("EPrime -> ''");
-    analyzer.parse("T -> F TPrime");
-    analyzer.parse("TPrime -> * F TPrime");
-    analyzer.parse("TPrime -> ''");
-    analyzer.parse("F -> ( E )");
-    analyzer.parse("F -> id");
-    fprintf(stdout, "Test synthatic variables: ");
-    compare_lists(analyzer.getVariables(), test1p);
-    fprintf(stdout, "Test terminals: ");
-    compare_lists(analyzer.getTerminals(), test1t);
-    fprintf(stdout, "\n");
+  fprintf(stdout, "===================== TEST 01 =====================\n");
+  analyzer.parse({
+  "E -> T EPrime",
+  "EPrime -> + T EPrime",
+  "EPrime -> ''",
+  "T -> F TPrime",
+  "TPrime -> * F TPrime",
+  "TPrime -> ''",
+  "F -> ( E )",
+  "F -> id"});
 
-    analyzer.clear();
+  // Terminals and not terminals
+  fprintf(stdout, "Test synthatic variables: ");
+  compare_lists(analyzer.getVariables(), {"E", "EPrime", "T", "TPrime", "F"});
+  fprintf(stdout, "Test terminals: ");
+  compare_lists(analyzer.getTerminals(), {"+", "*", "(", ")", "id"});
 
-    fprintf(stdout, "===================== TEST 02 =====================\n");
-    std::list<std::string> test2p = {"E", "T", "F"};
-    std::list<std::string> test2t = {"+", "*", "(", ")", "id"};
-    analyzer.parse("E -> E + T");
-    analyzer.parse("E -> T");
-    analyzer.parse("T -> T * F");
-    analyzer.parse("T -> F");
-    analyzer.parse("F -> id");
-    analyzer.parse("F -> ( E )");
-    fprintf(stdout, "Test synthatic variables: ");
-    compare_lists(analyzer.getVariables(), test2p);
-    fprintf(stdout, "Test terminals: ");
-    compare_lists(analyzer.getTerminals(), test2t);
-    fprintf(stdout, "\n");
+  // Firsts
+  fprintf(stdout, "Test FIRST(E): ");
+  compare_lists(analyzer.getFirst("E"), {"(", "id"});
+  fprintf(stdout, "Test FIRST(EPrime): ");
+  compare_lists(analyzer.getFirst("EPrime"), {"+", "''"});
+  fprintf(stdout, "Test FIRST(T): ");
+  compare_lists(analyzer.getFirst("T"), {"(", "id"});
+  fprintf(stdout, "Test FIRST(TPrime): ");
+  compare_lists(analyzer.getFirst("TPrime"), {"*", "''"});
+  fprintf(stdout, "Test FIRST(F): ");
+  compare_lists(analyzer.getFirst("F"), {"(", "id"});
 
-    analyzer.clear();
+  // Follows
+  fprintf(stdout, "Test FOLLOW(E): ");
+  compare_lists(analyzer.getFollow("E"), {"$", ")"});
+  fprintf(stdout, "Test FOLLOW(EPrime): ");
+  compare_lists(analyzer.getFollow("EPrime"), {"$", ")"});
+  fprintf(stdout, "Test FOLLOW(T): ");
+  compare_lists(analyzer.getFollow("T"), {"+", "$", ")"});
+  fprintf(stdout, "Test FOLLOW(TPrime): ");
+  compare_lists(analyzer.getFollow("TPrime"), {"+", "$", ")"});
+  fprintf(stdout, "Test FOLLOW(F): ");
+  compare_lists(analyzer.getFollow("F"), {"*", "+", "$", ")"});
+  fprintf(stdout, "\n");
 
-    fprintf(stdout, "===================== TEST 03 =====================\n");
-    std::list<std::string> test3p = {"A", "B", "C", "D"};
-    std::list<std::string> test3t = {"a", "b"};
-    analyzer.parse("A -> a A");
-    analyzer.parse("A -> b A");
-    analyzer.parse("A -> a B");
-    analyzer.parse("B -> b C");
-    analyzer.parse("C -> b D");
-    analyzer.parse("D -> ''");
-    fprintf(stdout, "Test synthatic variables: ");
-    compare_lists(analyzer.getVariables(), test3p);
-    fprintf(stdout, "Test terminals: ");
-    compare_lists(analyzer.getTerminals(), test3t);
-    fprintf(stdout, "\n");
+  analyzer.clear();
 
-    analyzer.clear();
+  fprintf(stdout, "===================== TEST 02 =====================\n");
+  analyzer.parse({
+  "E -> E + T",
+  "E -> T",
+  "T -> T * F",
+  "T -> F",
+  "F -> id",
+  "F -> ( E )"});
+  // Terminals and not terminals
+  fprintf(stdout, "Test synthatic variables: ");
+  compare_lists(analyzer.getVariables(), {"E", "T", "F"});
+  fprintf(stdout, "Test terminals: ");
+  compare_lists(analyzer.getTerminals(), {"+", "*", "(", ")", "id"});
 
-    fprintf(stdout, "===================== TEST 04 =====================\n");
-    std::list<std::string> test4p = {"bexpr", "bterm", "bfactor"};
-    std::list<std::string> test4t = {"or", "and", "not", "(", ")", "true", "false"};
-    analyzer.parse("bexpr -> bexpr or bterm");
-    analyzer.parse("bexpr -> bterm");
-    analyzer.parse("bterm -> bterm and bfactor");
-    analyzer.parse("bterm -> bfactor");
-    analyzer.parse("bfactor -> not bfactor");
-    analyzer.parse("bfactor -> ( bexpr )");
-    analyzer.parse("bfactor -> true");
-    analyzer.parse("bfactor -> false");
-    fprintf(stdout, "Test synthatic variables: ");
-    compare_lists(analyzer.getVariables(), test4p);
-    fprintf(stdout, "Test terminals: ");
-    compare_lists(analyzer.getTerminals(), test4t);
-    fprintf(stdout, "\n");
+  // Firsts
+  fprintf(stdout, "Test FIRST(E): ");
+  compare_lists(analyzer.getFirst("E"), {"id", "("});
+  fprintf(stdout, "Test FIRST(T): ");
+  compare_lists(analyzer.getFirst("T"), {"id", "("});
+  fprintf(stdout, "Test FIRST(F): ");
+  compare_lists(analyzer.getFirst("F"), {"id", "("});
 
-    analyzer.clear();
+  // Follows
+  fprintf(stdout, "Test FOLLOW(E): ");
+  compare_lists(analyzer.getFollow("E"), {"$", "+"});
+  fprintf(stdout, "Test FOLLOW(T): ");
+  compare_lists(analyzer.getFollow("T"), {"*", "$", "+"});
+  fprintf(stdout, "Test FOLLOW(F): ");
+  compare_lists(analyzer.getFollow("F"), {"*", "$", "+"});
+  fprintf(stdout, "\n");
 
-    fprintf(stdout, "===================== TEST 05 =====================\n");
-    std::list<std::string> test5p = {"S", "A", "APrime"};
-    std::list<std::string> test5t = {"a", "b", "c", "d"};
-    analyzer.parse("S -> A a");
-    analyzer.parse("S -> b");
-    analyzer.parse("A -> b d APrime");
-    analyzer.parse("A -> APrime");
-    analyzer.parse("APrime -> c APrime");
-    analyzer.parse("APrime -> a d APrime");
-    analyzer.parse("APrime -> ''");
-    fprintf(stdout, "Test synthatic variables: ");
-    compare_lists(analyzer.getVariables(), test5p);
-    fprintf(stdout, "Test terminals: ");
-    compare_lists(analyzer.getTerminals(), test5t);
-    fprintf(stdout, "\n");
+  analyzer.clear();
 
+  fprintf(stdout, "===================== TEST 03 =====================\n");
+  analyzer.parse({
+  "A -> a A",
+  "A -> b A",
+  "A -> a B",
+  "B -> b C",
+  "C -> b D",
+  "D -> ''"});
+  // Terminals and not Terminals
+  fprintf(stdout, "Test synthatic variables: ");
+  compare_lists(analyzer.getVariables(), {"A", "B", "C", "D"});
+  fprintf(stdout, "Test terminals: ");
+  compare_lists(analyzer.getTerminals(), {"a", "b"});
 
-    // Test first and follows
-    return 0;
+  // Firsts
+  fprintf(stdout, "Test FIRST(A): ");
+  compare_lists(analyzer.getFirst("A"), {"a", "b"});
+  fprintf(stdout, "Test FIRST(B): ");
+  compare_lists(analyzer.getFirst("B"), {"b"});
+  fprintf(stdout, "Test FIRST(C): ");
+  compare_lists(analyzer.getFirst("C"), {"b"});
+  fprintf(stdout, "Test FIRST(D): ");
+  compare_lists(analyzer.getFirst("D"), {"''"});
+
+  // Follows
+  fprintf(stdout, "Test FOLLOW(A): ");
+  compare_lists(analyzer.getFollow("A"), {"$"});
+  fprintf(stdout, "Test FOLLOW(B): ");
+  compare_lists(analyzer.getFollow("B"), {"$"});
+  fprintf(stdout, "Test FOLLOW(C): ");
+  compare_lists(analyzer.getFollow("C"), {"$"});
+  fprintf(stdout, "Test FOLLOW(D): ");
+  compare_lists(analyzer.getFollow("D"), {"$"});
+  fprintf(stdout, "\n");
+
+  analyzer.clear();
+
+  fprintf(stdout, "===================== TEST 04 =====================\n");
+  analyzer.parse({
+  "bexpr -> bexpr or bterm",
+  "bexpr -> bterm",
+  "bterm -> bterm and bfactor",
+  "bterm -> bfactor",
+  "bfactor -> not bfactor",
+  "bfactor -> ( bexpr )",
+  "bfactor -> true",
+  "bfactor -> false"});
+  // No terminals and terminals
+  fprintf(stdout, "Test synthatic variables: ");
+  compare_lists(analyzer.getVariables(), {"bexpr", "bterm", "bfactor"});
+  fprintf(stdout, "Test terminals: ");
+  compare_lists(analyzer.getTerminals(), {"or", "and", "not", "(", ")", "true", "false"});
+
+  // Firsts
+  fprintf(stdout, "Test FIRST(bexpr): ");
+  compare_lists(analyzer.getFirst("bexpr"), {"not", "(", "true", "false"});
+  fprintf(stdout, "Test FIRST(bterm): ");
+  compare_lists(analyzer.getFirst("bterm"), {"not", "(", "true", "false"});
+  fprintf(stdout, "Test FIRST(bfactor): ");
+  compare_lists(analyzer.getFirst("bfactor"), {"not", "(", "true", "false"});
+
+  // Follows
+  fprintf(stdout, "Test FOLLOW(bexpr): ");
+  compare_lists(analyzer.getFollow("bexpr"), {"$", "or", ")"});
+  fprintf(stdout, "Test FOLLOW(bterm): ");
+  compare_lists(analyzer.getFollow("bterm"), {"$", "or", ")", "and"});
+  fprintf(stdout, "Test FOLLOW(bfactor): ");
+  compare_lists(analyzer.getFollow("bfactor"), {"$", "or", ")", "and"});
+  fprintf(stdout, "\n");
+
+  analyzer.clear();
+
+  fprintf(stdout, "===================== TEST 05 =====================\n");
+  analyzer.parse({
+  "S -> A a",
+  "S -> b",
+  "A -> b d APrime",
+  "A -> APrime",
+  "APrime -> c APrime",
+  "APrime -> a d APrime",
+  "APrime -> ''"});
+  // No Terminals and Terminals
+  fprintf(stdout, "Test synthatic variables: ");
+  compare_lists(analyzer.getVariables(), {"S", "A", "APrime"});
+  fprintf(stdout, "Test terminals: ");
+  compare_lists(analyzer.getTerminals(), {"a", "b", "c", "d"});
+  fprintf(stdout, "\n");
+
+  // Firsts
+  fprintf(stdout, "Test FIRST(S): ");
+  compare_lists(analyzer.getFirst("S"), {"b", "c", "a", "''"});
+  fprintf(stdout, "Test FIRST(A): ");
+  compare_lists(analyzer.getFirst("A"), {"b", "c", "a", "''"});
+  fprintf(stdout, "Test FIRST(APrime): ");
+  compare_lists(analyzer.getFirst("APrime"), {"c", "a", "''"});
+
+  // Follows
+  fprintf(stdout, "Test FOLLOW(S): ");
+  compare_lists(analyzer.getFollow("S"), {"$"});
+  fprintf(stdout, "Test FOLLOW(A): ");
+  compare_lists(analyzer.getFollow("A"), {});
+  fprintf(stdout, "Test FOLLOW(bfactor): ");
+  compare_lists(analyzer.getFollow("APrime"), {});
+  fprintf(stdout, "\n");
+
+  return 0;
 }
