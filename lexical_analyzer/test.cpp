e01428066c4b284d58d53eda04738a30d2149e5c
peterglezroa <peterglezroa@gmail.com>
2021-10-16 23:13:01 -0500

    Improve tests for lexical_analyzer

--- lexical_analyzer/test.cpp
+++ lexical_analyzer/test.cpp
@@ -1,49 +1,53 @@
 #include <iterator>
 #include <list>
+#include <queue>
 #include <string>
-#include <vector>
 #include "lexical_analyzer.h"
 
-bool has_same_strings(const std::list<std::string> list, const std::vector<std::string> vector) {
-    bool found = false;
-    if (list.size() != vector.size())
-        return false;
-    for (std::string str : vector)
-        if (std::find(list.begin(), list.end(), str) == list.end())
-            return false;
-    return true;
-}
+void compare_lists (
+    const std::list<std::string> list1,
+    const std::list<std::string> list2
+){
+    std::queue<std::string> missing; // List of words missing
 
-void test(const LexicalAnalyzer analyzer, std::vector<std::string> vars, std::vector<std::string> terms) {
-    fprintf(stdout, "( ");
-    for (std::string str : analyzer.getVariables())
-        fprintf(stdout, "\033[1;35m%s \033[0m", str.c_str());
-    fprintf(stdout, ") and ( ");
-    for (std::string str : vars)
-        fprintf(stdout, "\033[1;35m%s \033[0m", str.c_str());
-    fprintf(stdout, "): ");
-    (has_same_strings(analyzer.getVariables(), vars))?
-        fprintf(stdout, "\033[1;32mCORRECT\033[0m\n"):
-        fprintf(stdout, "\033[1;31mINCORRECT\033[0m\n");
+    // Find items of list 1 in list 2
+    for (std::string str : list1)
+        if (std::find(list2.begin(), list2.end(), str) == list2.end())
+            missing.push(str);
 
-    fprintf(stdout, "( ");
-    for (std::string str : analyzer.getVariables())
-        fprintf(stdout, "\033[1;35m%s \033[0m", str.c_str());
-    fprintf(stdout, ") and ( ");
-    for (std::string str : terms)
-        fprintf(stdout, "\033[1;35m%s \033[0m", str.c_str());
-    fprintf(stdout, "): ");
-    (has_same_strings(analyzer.getTerminals(), terms))?
-        fprintf(stdout, "\033[1;32mCORRECT\033[0m\n"):
-        fprintf(stdout, "\033[1;31mINCORRECT\033[0m\n");
+    // Find items of list 2 in list 1
+    for (std::string str : list2)
+        if (std::find(list1.begin(), list1.end(), str) == list1.end())
+            missing.push(str);
+
+    if (missing.size() > 0) {
+        fprintf(stdout, "\033[1;31mINCORRECT\033[0m\n\t");
+
+        fprintf(stdout, "( ");
+        std::string top = missing.front(); missing.pop();
+        for (const std::string str : list1)
+            if (str.compare(top) == 0) {
+                fprintf(stdout, "\033[1;35m%s \033[0m", str.c_str());
+                if (!missing.empty()) { top = missing.front(); missing.pop(); }
+            } else fprintf(stdout, "%s ", str.c_str());
+        fprintf(stdout, ")");
+
+        fprintf(stdout, "\t( ");
+        for (const std::string str : list2)
+            if (str.compare(top) == 0) {
+                fprintf(stdout, "\033[1;35m%s \033[0m", str.c_str());
+                if (!missing.empty()) { top = missing.front(); missing.pop(); }
+            } else fprintf(stdout, "%s ", str.c_str());
+        fprintf(stdout, ")\n");
+    } else fprintf(stdout, "\033[1;32mCORRECT\033[0m\n");
 }
 
 int main(int argc, char *argv[]) {
     LexicalAnalyzer analyzer = LexicalAnalyzer();
 
-
-    std::vector<std::string> test1p = {"E", "EPrime", "T", "TPrime", "F"};
-    std::vector<std::string> test1t = {"+", "*", "(", ")", "id"};
+    fprintf(stdout, "===================== TEST 01 =====================\n");
+    std::list<std::string> test1p = {"E", "EPrime", "T", "TPrime", "F"};
+    std::list<std::string> test1t = {"+", "*", "(", ")", "id"};
     analyzer.parse("E -> T EPrime");
     analyzer.parse("EPrime -> + T EPrime");
     analyzer.parse("EPrime -> ''");
@@ -52,39 +56,51 @@ int main(int argc, char *argv[]) {
     analyzer.parse("TPrime -> ''");
     analyzer.parse("F -> ( E )");
     analyzer.parse("F -> id");
-    test(analyzer, test1p, test1t);
+    fprintf(stdout, "Test synthatic variables: ");
+    compare_lists(analyzer.getVariables(), test1p);
+    fprintf(stdout, "Test terminals: ");
+    compare_lists(analyzer.getTerminals(), test1t);
     fprintf(stdout, "\n");
 
     analyzer.clear();
 
-    std::vector<std::string> test2p = {"E", "T", "F"};
-    std::vector<std::string> test2t = {"+", "*", "(", ")", "id"};
+    fprintf(stdout, "===================== TEST 02 =====================\n");
+    std::list<std::string> test2p = {"E", "T", "F"};
+    std::list<std::string> test2t = {"+", "*", "(", ")", "id"};
     analyzer.parse("E -> E + T");
     analyzer.parse("E -> T");
     analyzer.parse("T -> T * F");
     analyzer.parse("T -> F");
     analyzer.parse("F -> id");
     analyzer.parse("F -> ( E )");
-    test(analyzer, test2p, test2t);
+    fprintf(stdout, "Test synthatic variables: ");
+    compare_lists(analyzer.getVariables(), test2p);
+    fprintf(stdout, "Test terminals: ");
+    compare_lists(analyzer.getTerminals(), test2t);
     fprintf(stdout, "\n");
 
     analyzer.clear();
 
-    std::vector<std::string> test3p = {"A", "B", "C", "D"};
-    std::vector<std::string> test3t = {"a", "b"};
+    fprintf(stdout, "===================== TEST 03 =====================\n");
+    std::list<std::string> test3p = {"A", "B", "C", "D"};
+    std::list<std::string> test3t = {"a", "b"};
     analyzer.parse("A -> a A");
     analyzer.parse("A -> b A");
     analyzer.parse("A -> a B");
     analyzer.parse("B -> b C");
     analyzer.parse("C -> b D");
     analyzer.parse("D -> ''");
-    test(analyzer, test3p, test3t);
+    fprintf(stdout, "Test synthatic variables: ");
+    compare_lists(analyzer.getVariables(), test3p);
+    fprintf(stdout, "Test terminals: ");
+    compare_lists(analyzer.getTerminals(), test3t);
     fprintf(stdout, "\n");
 
     analyzer.clear();
 
-    std::vector<std::string> test4p = {"bexpr", "bterm", "bfactor"};
-    std::vector<std::string> test4t = {"or", "and", "not", "(", ")", "true", "false"};
+    fprintf(stdout, "===================== TEST 04 =====================\n");
+    std::list<std::string> test4p = {"bexpr", "bterm", "bfactor"};
+    std::list<std::string> test4t = {"or", "and", "not", "(", ")", "true", "false"};
     analyzer.parse("bexpr -> bexpr or bterm");
     analyzer.parse("bexpr -> bterm");
     analyzer.parse("bterm -> bterm and bfactor");
@@ -93,13 +109,17 @@ int main(int argc, char *argv[]) {
     analyzer.parse("bfactor -> ( bexpr )");
     analyzer.parse("bfactor -> true");
     analyzer.parse("bfactor -> false");
-    test(analyzer, test4p, test4t);
+    fprintf(stdout, "Test synthatic variables: ");
+    compare_lists(analyzer.getVariables(), test4p);
+    fprintf(stdout, "Test terminals: ");
+    compare_lists(analyzer.getTerminals(), test4t);
     fprintf(stdout, "\n");
 
     analyzer.clear();
 
-    std::vector<std::string> test5p = {"S", "A", "APrime"};
-    std::vector<std::string> test5t = {"a", "b", "c", "d"};
+    fprintf(stdout, "===================== TEST 05 =====================\n");
+    std::list<std::string> test5p = {"S", "A", "APrime"};
+    std::list<std::string> test5t = {"a", "b", "c", "d"};
     analyzer.parse("S -> A a");
     analyzer.parse("S -> b");
     analyzer.parse("A -> b d APrime");
@@ -107,7 +127,11 @@ int main(int argc, char *argv[]) {
     analyzer.parse("APrime -> c APrime");
     analyzer.parse("APrime -> a d APrime");
     analyzer.parse("APrime -> ''");
-    test(analyzer, test5p, test5t);
+    fprintf(stdout, "Test synthatic variables: ");
+    compare_lists(analyzer.getVariables(), test5p);
+    fprintf(stdout, "Test terminals: ");
+    compare_lists(analyzer.getTerminals(), test5t);
+    fprintf(stdout, "\n");
 
 
     // Test first and follows
